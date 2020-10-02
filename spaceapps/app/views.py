643b1d6de16e05ef14ca7e9c64e783ebf2ca7a76
alilmalki <A.Lmalki@aui.ma>
2020-10-02 10:22:48 +0100

    added login logout and register

--- spaceapps/app/views.py
+++ spaceapps/app/views.py
@@ -20,3 +20,54 @@ def index(request):
 
 def home(request):
     return render(request, "app/home.html")
+
+@csrf_exempt
+def login_view(request):
+    
+    if request.method == "POST":
+
+        data = json.loads(request.body)
+
+        username = data.get("username", "")
+        password = data.get("password", "")
+
+        user = authenticate(request, username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": "Loged in"}, status = 201)  # I didn't return HttpResponseRedirect because fetch calls do not accept such returns
+        else:
+            return JsonResponse({"message": "Invalid username or Password"})
+
+    else:
+        pass
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["name"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        
+        if password != confirmation:
+            return JsonResponse({"message": "Password unmatches confirmation!"})
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        except IntegrityError:
+            return JsonResponse({"message": "Username/email/password already taken"})
+        login(request, user)
+        return HttpResponseRedirect(reverse("mainPage"))  # got to main page
+    else:
+        pass
