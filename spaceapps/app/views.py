6ad411674841eb87dae51e27989b98f0ce92aa5f
alilmalki <A.Lmalki@aui.ma>
2020-10-02 10:00:18 +0100

    Added index and mainPage

--- spaceapps/app/views.py
+++ spaceapps/app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.http import JsonResponse
+import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("mainPage"))
+    return render(request, "app/index.html")
+
+def mainPage(request):
+    return render(request, "app/mainPage.html")
